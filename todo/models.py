a719e926227622ca8e10304bae975d690c29dc4b
codscript <mussa.monteiro@gmail.com>
2021-09-05 17:57:50 -0300

    modificações de uso

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Tarefa(models.Model):
+    tarefa = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.tarefa
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .models import Tarefa
+from .forms import *
 
-# Create your views here.
+tarefas = ['aprender django', 'estudar selenium', 'classes online']
+
+def home(request):
+    tarefas = Tarefa.objects.all()
+    context = { 'tarefas': tarefas }
+    return render(request, 'todo/home.html', context)
+
+def add(request):
+    if request.method == 'POST':
+        form = AdicionarTarefa(request.POST)
+        if form.is_valid():
+            # tarefa = form.cleaned_data["tarefa"]
+            # print(tarefa)
+            form.save()
+            return redirect("home")
+    else:
+        form = AdicionarTarefa()
+    context = {'form': form}
+    return render(request, 'todo/add.html', context)
+
+def deletar(request, tarefa_id):
+    tarefa = Tarefa.objects.get(id=tarefa_id)
+    tarefa.delete()
+    return redirect("home")
+
+def editar(request, tarefa_id):
+    tarefa = Tarefa.objects.get(id=tarefa_id)
+    if request.method == 'POST':
+        form = AdicionarTarefa(request.POST, instance=tarefa)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+    else:
+        form = AdicionarTarefa(instance=tarefa)
+    context = {'form': form}
+    return render(request, 'todo/editar.html', context)
