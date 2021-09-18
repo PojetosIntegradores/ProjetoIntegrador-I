76eb80f92e9b99162f72dd9a5f6d9d8d1a14a300
codscript <mussa.monteiro@gmail.com>
2021-09-18 05:48:01 -0300

    update e delete funcionalidades crud concluidas

--- base/views.py
+++ base/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
 from .models import Task
@@ -20,6 +20,18 @@ class TaskDetail(DetailView):
     template_name = 'base/task.html'
 
 class TaskCreate(CreateView):
+    model = Task
+    context_object_name = 'task'
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+class TaskUpdate(UpdateView):
+    model = Task
+    context_object_name = 'task'
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+class TaskDelete(DeleteView):
     model = Task
     context_object_name = 'task'
     fields = '__all__'
